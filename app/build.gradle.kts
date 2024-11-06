6e0f6a7b5decdb800475c00bfe113018822b72ae
KimYunah <124032517+KimYunah@users.noreply.github.com>
2024-11-06 19:06:34 +0900

    프로젝트 생성
    프로젝트 생성 및 사용 라이브러리 추가

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.kya.newsapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.kya.newsapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // ViewModel
+    implementation(libs.lifecycle.viewmodel)
+    // by viewModels() 사용을 위한 Activity KTX, Fragment KTX
+    implementation(libs.activity.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // Retrofit2
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // Coil(이미지 로더)
+    implementation(libs.coil)
+}
